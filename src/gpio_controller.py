8fd70c73dbcf788e57571e40229bfbf47242ee0d
nilton667 <nilton667@gmail.com>
2025-08-26 10:54:42 +0100

    0.7.2

--- src/gpio_controller.py
+++ src/gpio_controller.py
@@ -19,7 +19,7 @@ if RUNNING_ON_PI:
     def desativar_relay(gpio_number: int):
         try:
             gpio = GPIO(gpio_number, "out")
-            gpio.write(False)
+            gpio.write(True)
             gpio.close()
         except Exception as e:
             print(f"Erro ao desativar o relé no GPIO {gpio_number}: {e}")
